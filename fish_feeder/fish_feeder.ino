120c6ed14496824111d005a8b0b556b0d06ef97b
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-24 08:17:34 +0800

    feat: add motor power and servo angle stat

--- fish_feeder/fish_feeder.ino
+++ fish_feeder/fish_feeder.ino
@@ -17,19 +17,17 @@ struct {
       time += "0";
     }
     time += String(this->hour);
-    time += ":";
-    if (this->minute < 10) {
-      time += "0";
-    }
-    time += String(this->minute);
     // time += ":";
-    // if (this->second < 10) {
+    // if (this->minute < 10) {
     //   time += "0";
     // }
-    // time += String(this->second);
+    // time += String(this->minute);
     return time;
   }
 } Time1, Time2;
+
+int motorPower = 255;
+int servoAngle = 90;
 
 const int buttonPin8 = 8;
 const int buttonPin7 = 7;
@@ -45,7 +43,7 @@ unsigned long lastDebounceTime6 = 0;
 
 const unsigned long debounceDelay = 200;
 
-bool isEditingTime1 = false;
+int editIndex = 1;
 
 void setup() {
   Serial.begin(9600);
@@ -77,11 +75,11 @@ void setup() {
 }
 
 void loop() {
-  // LCD
+  // Display
   DateTime now = rtc.now();
 
   lcd.setCursor(0, 0);
-  lcd.print("    ");
+  lcd.print("  ");
   if (now.hour() < 10) {
     lcd.print("0");
   }
@@ -97,78 +95,110 @@ void loop() {
     lcd.print("0");
   }
   lcd.print(now.second());
-  if (isEditingTime1) {
-    lcd.print(" - 1");
+  if (editIndex == 1) {
+    lcd.print(" [1]");
+  } else if (editIndex == 2) {
+    lcd.print(" [2]");
+  }  else if(editIndex == 3) {
+    lcd.print(" [3]");
   } else {
-    lcd.print(" - 2");
+    lcd.print(" [4]");
   }
 
   lcd.setCursor(0, 1);
-  lcd.print(" ");
+  lcd.print("  ");
   lcd.print(Time1.getString());
-  lcd.print("   ");
+  lcd.print(" ");
   lcd.print(Time2.getString());
+  lcd.print(" ");
+  if(motorPower < 100) {
+    lcd.print("0");
+  }
+  lcd.print(String(motorPower));
+  lcd.print(" ");
+  lcd.print(String(servoAngle));
 
-  // Push buttons
+  // Buttons
   int reading8 = digitalRead(buttonPin8);
   int reading7 = digitalRead(buttonPin7);
   int reading6 = digitalRead(buttonPin6);
 
-  // Check for button press on pin D8
   if (reading8 != lastButtonState8) {
     lastDebounceTime8 = millis();
   }
   if (reading8 == LOW) {
     if ((millis() - lastDebounceTime8) > debounceDelay) {
       Serial.println("Button on pin D8 pressed");
-      isEditingTime1 = !isEditingTime1;
-      lastDebounceTime8 = millis(); // Reset debounce timer
+      if(editIndex == 4) {
+        editIndex = 0;
+      } else {
+        editIndex += 1;
+      }
+      lastDebounceTime8 = millis();
     }
   }
   lastButtonState8 = reading8;
 
-  // Check for button press on pin D7
   if (reading7 != lastButtonState7) {
     lastDebounceTime7 = millis();
   }
   if (reading7 == LOW) {
     if ((millis() - lastDebounceTime7) > debounceDelay) {
       Serial.println("Button on pin D7 pressed");
-      if (isEditingTime1) {
+      if (editIndex == 1) {
         Time1.hour -= 1;
         if (Time1.hour < 0) {
           Time1.hour = 23;
         }
-      } else {
+      } else if (editIndex == 2) {
         Time2.hour -= 1;
         if (Time2.hour < 0) {
           Time2.hour = 23;
         }
+      } else if (editIndex == 3) {
+        motorPower -= 10;
+        if (motorPower < 10) {
+          motorPower = 255;
+        } 
+      } else {
+        servoAngle -= 10;
+        if (servoAngle < 10) {
+          servoAngle = 90;
+        }
       }
-      lastDebounceTime7 = millis(); // Reset debounce timer
+      lastDebounceTime7 = millis();
     }
   }
   lastButtonState7 = reading7;
 
-  // Check for button press on pin D6
   if (reading6 != lastButtonState6) {
     lastDebounceTime6 = millis();
   }
   if (reading6 == LOW) {
     if ((millis() - lastDebounceTime6) > debounceDelay) {
       Serial.println("Button on pin D6 pressed");
-      if (isEditingTime1) {
+      if (editIndex == 1) {
         Time1.hour += 1;
         if (Time1.hour > 23) {
           Time1.hour = 0;
         }
-      } else {
+      } else if (editIndex == 2) {
         Time2.hour += 1;
         if (Time2.hour > 23) {
           Time2.hour = 0;
         }
+      } else if (editIndex == 3) {
+        motorPower += 10;
+        if (motorPower > 255) {
+          motorPower = 10;
+        }
+      } else {
+        servoAngle += 10;
+        if (servoAngle > 90) {
+          servoAngle = 10;
+        }
       }
-      lastDebounceTime6 = millis(); // Reset debounce timer
+      lastDebounceTime6 = millis();
     }
   }
   lastButtonState6 = reading6;
